ce87f9ab6421d624cdb9f708c8bac5700a4319fa
Shivam2004-coder <shivamgit003@gmail.com>
2025-06-18 19:49:07 +0530

    scrolling added in the profileDetail

--- routes/users.js
+++ routes/users.js
@@ -4,7 +4,7 @@ const {userAuth} = require("../middlewares/auth");
 const ConnectionRequestModel = require("../models/connectionRequestSchema");
 const User = require("../models/user");
 
-const USER_SAFE_DATA = [ "firstName" , "lastName" , "dateOfBirth" , "gender" , "livingIn" , "profileImage" , "uploadedImages" , "bio" , "jobTitle" , "companyName" , "school", "skills" , "socialLinks" ];
+const USER_SAFE_DATA = [ "firstName" , "lastName" , "dateOfBirth" , "gender" , "promptContent" , "livingIn" , "profileImage" , "uploadedImages" , "bio" , "jobTitle" , "companyName" , "school", "skills" , "socialLinks" ];
 
 // Get all the pending connection requests for the loggedIn User
 userRouter.get("/user/requests/received" , userAuth , async (req,res) => {
